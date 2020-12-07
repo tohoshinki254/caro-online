a61e7d0d209a9438b409f0fca359d888beb73111
DESKTOP-576RT13\quang <quangthien.tiit@gmail.com>
2020-12-07 23:31:52 +0700

    user: +Room and ListRoom component.

--- caro-user/src/components/Room/index.js
+++ caro-user/src/components/Room/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import { Grid, makeStyles, Typography } from '@material-ui/core';
+import {makeStyles, Typography } from '@material-ui/core';
 
 const Room = ({room}) => {
     const classes = useStyle();
     return (
         <div className={classes.container}>
-            <img className={classes.image} src="/assets/images/board-room.png"/>
+            <img className={classes.image} src="/assets/images/board-room.png" alt="room"/>
             <Typography>ID: 123</Typography>
             <Typography>Let's play Tic Tac Toe</Typography>
         </div>
--- caro-user/src/containers/HomePage/index.js
+++ caro-user/src/containers/HomePage/index.js
@@ -1,7 +1,11 @@
 import React from 'react';
 import { Grid, makeStyles, Typography } from '@material-ui/core';
 import MyAppBar from '../../components/MyAppBar';
-import Room from '../../components/Room';
+import ListRoom from './ListRoom';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import MyTextField from '../../components/MyTextField';
+import MyButton from '../../components/MyButton';
+import SearchIcon from '@material-ui/icons/Search';
 
 const HomePage = () => {
     const classes = useStyle();
@@ -15,19 +19,34 @@ const HomePage = () => {
             </Grid>
             <Grid className={classes.mainSection} container>
                 <Grid className={classes.leftSection} item xs={8}>
+                    <Grid style={{marginTop: '1%'}} container>
+                        <Grid item xs={6}>
+                            <MyTextField
+                                className={classes.searchBar} 
+                                fullWidth
+                                InputProps={{
+                                    startAdornment: (
+                                      <InputAdornment position="start">
+                                        <SearchIcon style={{fontSize: '1.3rem'}} />
+                                      </InputAdornment>
+                                    ),
+                                }}
+                                placeholder={'Search room by ID'}
+                            />
+                        </Grid>
+                        <Grid item xs={3} >
+                            <MyButton className={classes.button}>
+                                Create Room
+                            </MyButton>
+                        </Grid>
+                        <Grid item xs={3}>
+                            <MyButton className={classes.button}>
+                                Play now
+                            </MyButton>
+                        </Grid>
+                    </Grid>
                     <Grid container>
-                        <Grid item xs={3}>
-                            <Room />
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Room />
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Room />
-                        </Grid>
-                        <Grid item xs={3}>
-                            <Room />
-                        </Grid>
+                        <ListRoom />
                     </Grid>
                 </Grid>
                 <Grid item xs={4}>
@@ -54,7 +73,20 @@ const useStyle = makeStyles({
         borderColor: 'darkgray',
         borderWidth: '2px',
         borderStyle: 'solid',
-        borderRadius: '5px'
+        borderRadius: '5px',
+        paddingBottom: '2%',
+    },
+    searchBar: {
+        width: '60%',
+        marginTop: '2%',
+        marginLeft: '8%',
+        marginBottom: '8%'
+    },
+    button: {
+        marginTop: '4%',
+        marginLeft: '8%',
+        marginBottom: '8%',
+        width: '60% '
     }
 });
 export default HomePage;
