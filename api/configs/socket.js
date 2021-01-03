b959090274365133edc6d5ca1dada9d56f94ceaa
DESKTOP-576RT13\quang <quangthien.tiit@gmail.com>
2021-01-03 21:58:52 +0700

    + api: update result match by socket.

--- api/configs/socket.js
+++ api/configs/socket.js
@@ -2,6 +2,7 @@ const accountDAO = require('../models/account');
 const roomDAO = require('../models/room');
 const moment = require('moment');
 const cookieParser = require('cookie-parser');
+const { updateResult } = require('../controllers/room');
 
 const getOnlineList = async () => {
   const res = await accountDAO.find({ isOnline: true, isAdmin: false });
@@ -34,10 +35,12 @@ module.exports = {
       }
     });
 
-    socket.on('join-room', (data) => {
+    socket.on('join-room', async (data) => {
       socket.join(data.roomId);
       if (!data.isCreator) {
-        socket.to(`${data.roomId}`).emit('player-joined', data.name);
+        const player = await accountDAO.findById(data.userId);
+        if (player)
+          socket.to(`${data.roomId}`).emit('player-joined', player);
       }
     });
 
@@ -57,21 +60,25 @@ module.exports = {
     })
 
     //isWin true: win, false: draw
-    socket.on('result', async ({isWin, roomId, isCreator}) => {
+    socket.on('result', async ({ isWin, roomId, isCreator }) => {
       //update db
-      const room = await roomDAO.findOne({roomId: roomId});
-      if (room){
-        if (isCreator && isWin){
+      const room = await roomDAO.findOne({ roomId: roomId });
+      if (room) {
+        if (isCreator && isWin) {
           room.creatorWinner++;
-        }else{
-          if (!isCreator && isWin){
+          await updateResult(roomId, 1);
+        } else {
+          if (!isCreator && isWin) {
             room.playerWinner++;
+            await updateResult(roomId, -1);
+          } else {
+            await updateResult(roomId, 0);
           }
         }
         await room.save();
       }
       const result = isWin ? -1 : 0;
-      socket.to(`${roomId}`).emit('game-done', {result: result});
+      socket.to(`${roomId}`).emit('game-done', { result: result });
     })
 
     //update list room realtime 
@@ -85,63 +92,83 @@ module.exports = {
     })
 
     //countdown creator
-    socket.on('countdown-creator', async ({remain, roomId}) => {
-      socket.to(`${roomId}`).emit('creator-remain-time', {remain: remain})
+    socket.on('countdown-creator', async ({ remain, roomId }) => {
+      socket.to(`${roomId}`).emit('creator-remain-time', { remain: remain })
       if (remain === 0) {
-        const room = await roomDAO.findOne({roomId: roomId});
+        const room = await roomDAO.findOne({ roomId: roomId });
         room.playerWinner++;
+        await updateResult(roomId, -1);
         await room.save();
       }
 
     })
 
     //countdown player
-    socket.on('countdown-player', async ({remain, roomId}) => {
-      socket.to(`${roomId}`).emit('player-remain-time', {remain: remain});
-      if (remain === 0){
-        const room = await roomDAO.findOne({roomId: roomId});
+    socket.on('countdown-player', async ({ remain, roomId }) => {
+      socket.to(`${roomId}`).emit('player-remain-time', { remain: remain });
+      if (remain === 0) {
+        const room = await roomDAO.findOne({ roomId: roomId });
         room.creatorWinner++;
+        await updateResult(roomId, 1);
         await room.save();
       }
     })
 
     //creator resign
-    socket.on('creator-resign', async ({roomId}) => {
+    socket.on('creator-resign', async ({ roomId }) => {
       socket.to(`${roomId}`).emit('creator-resigned');
-      const room = await roomDAO.findOne({roomId: roomId});
+      const room = await roomDAO.findOne({ roomId: roomId });
       if (room) {
         room.playerWinner++;
+        await updateResult(roomId, -1);
         await room.save();
       }
     })
 
     //player resign
-    socket.on('player-resign', async ({roomId}) => {
+    socket.on('player-resign', async ({ roomId }) => {
       socket.to(`${roomId}`).emit('player-resigned');
-      const room = await roomDAO.findOne({roomId: roomId});
+      const room = await roomDAO.findOne({ roomId: roomId });
       if (room) {
         room.creatorWinner++;
+        await updateResult(roomId, 1);
         await room.save();
       }
     })
 
     //creator claim draw
-    socket.on('creator-claim-draw', async ({roomId}) => {
+    socket.on('creator-claim-draw', async ({ roomId }) => {
       socket.to(`${roomId}`).emit('creator-claimed-draw');
     })
 
-    socket.on('player-reply-draw', async ({roomId, accept}) => {
-      socket.to(`${roomId}`).emit('player-replied-draw', {accept});
+    socket.on('player-reply-draw', async ({ roomId, accept }) => {
+      socket.to(`${roomId}`).emit('player-replied-draw', { accept });
+      if (accept) {
+        const room = await roomDAO.findOne({ roomId: roomId });
+        if (room) {
+          room.draws++;
+          await room.save();
+          await updateResult(roomId, 0);
+        }
+      }
     })
 
     //player claim draw
-    socket.on('player-claim-draw', async ({roomId}) => {
+    socket.on('player-claim-draw', async ({ roomId }) => {
       socket.to(`${roomId}`).emit('player-claimed-draw');
     })
 
 
-    socket.on('creator-reply-draw', async ({roomId, accept}) => {
-      socket.to(`${roomId}`).emit('creator-replied-draw', {accept});
+    socket.on('creator-reply-draw', async ({ roomId, accept }) => {
+      socket.to(`${roomId}`).emit('creator-replied-draw', { accept });
+      if (accept) {
+        const room = await roomDAO.findOne({ roomId: roomId });
+        if (room) {
+          room.draws++;
+          await room.save();
+          await updateResult(roomId, 0);
+        }
+      }
     })
   }
 }
--- api/controllers/room.js
+++ api/controllers/room.js
@@ -2,225 +2,293 @@ const accountDAO = require('../models/account');
 const roomDAO = require('../models/room');
 
 module.exports = {
-    getPublicRooms: async (req, res, next) => {
-        try {
-            const rooms = await roomDAO.find({ isPublic: true });
-            res.status(200).json({
-                message: 'Successful',
-                rooms: rooms
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            });
-        }
-    },
-
-    joinRoom: async (req, res, next) => {
-        try {
-            const roomId = req.body.roomId;
-            const playerId = req.user._id;
-
-            const room = await roomDAO.findOne({ roomId: roomId });
-            if (room === null) {
-                res.status(401).json({
-                    message: 'The room is not existed'
-                });
-                return;
-            }
-
-            if (room.player !== null){
-                res.status(403).json({
-                    message: 'The room is full.'
-                });
-                return;
-            }
-
-            room.player = playerId;
-            
-            await room.save();
-            res.status(200).json({
-                message: 'Successful'
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
-        }
-    },
-    
-    createRoom: async (req, res, next) => {
-        try {
-            let isPublic = req.body.isPublic;
-            const name = req.body.name;
-            if (isPublic === undefined) isPublic = true; 
-            if (name === undefined){
-                res.status(401).json({
-                    message: 'Name is not define.'
-                });
-                return;
-            }
-            const roomWithMaxId = await roomDAO.findOne().sort('-roomId');
-            const newRoomId = roomWithMaxId === null ? 1 : roomWithMaxId.roomId + 1;
-            const newRoom = new roomDAO({
-                roomId: newRoomId,
-                creator: req.user._id,
-                player: null,
-                isEnd: false,
-                creatorWinner: 0,
-                playerWinner: 0,
-                draws: 0,
-                createdTime: new Date(),
-                isPublic: isPublic,
-                name: name
-            });
-            await newRoom.save();
-            res.status(200).json({
-                message: 'OK',
-                roomId: newRoom.roomId
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
-        }
-    },
-
-    getDetailRoom: async (req, res, next) => {
-        try {
-            const roomId = req.body.roomId;
-            const userId = req.body.userId;
-            if (roomId === undefined || userId === undefined){
-                res.status(401).json({
-                    message: 'Room ID or User ID not defined'
-                });
-                return;
-            }
-
-            const room = await roomDAO.findOne({roomId: roomId});
-            if (room === null){
-                res.status(404).json({
-                    message: 'Room not found'
-                });
-                return;
-            }
-
-            let creator = {
-                name: 'N/A',
-                mark: 0
-            }
-            let player = {
-                name: 'N/A',
-                mark: 0
-            }
-            if (room.creator !== null) {
-                const _creator = await accountDAO.findById(room.creator);
-                creator = {name: _creator.name, mark: room.creatorWinner}
-            }
-
-            if (room.player !== null) {
-                const _player = await accountDAO.findById(room.player);
-                player = {name: _player.name, mark: room.playerWinner}
-            }
-
-            res.status(200).json({
-                creator: creator,
-                player: player,
-                isCreator: userId === room.creator
-            })
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            }); 
-        }
-    },
-
-    // Cập nhật thông số của người chơi
-    // Lưu chat, history
-    // Dùng roomId lấy các thông tin có sẵn rồi update thêm history, chat, ...
-    updateParamsAfterEnd: async (req, res, next) => {
-        try {
-            const { id, creatorWins, playerWins, draws, chat, history } = req.body;
-            
-            const room = await roomDAO.findOne({ roomId: id });
-            if (room === null || room === undefined) {
-                res.status(400).json({
-                    message: 'Room is not existed'
-                });
-                return;
-            }
-
-            const creator = await accountDAO.findById(room.creator);
-            const player = await accountDAO.findById(room.player);
-            if (creator === null || player === null) {
-                res.status(401).json({
-                    message: 'Account is not existed'
-                });
-                return;
-            }
-
-            room.creatorWinner = creatorWins;
-            room.playerWinner = playerWins;
-            room.draws = draws;
-            room.chat = chat;
-            room.history = history;
-            await room.save();
-
-            creator.wins += creatorWins;
-            creator.loses += playerWins;
-            creator.draws += draws;
-            await creator.save();
-
-            player.wins += playerWins;
-            player.loses += creatorWins;
-            player.draws += draws;
-            await player.save();
-
-            res.status(200).json({
-                message: 'OK'
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
-        }
-    },
-
-    getRoomByUserId: async (req, res, next) => {
-        try {
-            console.log(req.body);
-            const { id } = req.body;
-            const user = await accountDAO.findById(id);
-            if (user === null) {
-                res.status(400).json({
-                    message: 'User is not existed'
-                });
-                return;
-            }
-
-            const rooms = await roomDAO.find({ $or: [{ creator: id }, { player: id }] });
-            res.status(200).json({
-                message: 'OK',
-                rooms: rooms
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
-        }
-    },
-
-    getRoomsEnded: async (req, res, next) => {
-        try {
-            const rooms = await roomDAO.find({ isEnd: true });
-            res.status(200).json({
-                message: 'OK',
-                rooms: rooms
-            });
-        } catch (e) {
-            res.status(500).json({
-                message: e.message
-            })
-        }
+  getPublicRooms: async (req, res, next) => {
+    try {
+      const rooms = await roomDAO.find({ isPublic: true });
+      res.status(200).json({
+        message: 'Successful',
+        rooms: rooms
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      });
     }
+  },
+
+  joinRoom: async (req, res, next) => {
+    try {
+      const roomId = req.body.roomId;
+      const playerId = req.user._id;
+
+      const room = await roomDAO.findOne({ roomId: roomId });
+      if (room === null) {
+        res.status(401).json({
+          message: 'The room is not existed'
+        });
+        return;
+      }
+
+      if (room.player !== null) {
+        res.status(403).json({
+          message: 'The room is full.'
+        });
+        return;
+      }
+
+      room.player = playerId;
+
+      await room.save();
+      res.status(200).json({
+        message: 'Successful'
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      })
+    }
+  },
+
+  createRoom: async (req, res, next) => {
+    try {
+      let isPublic = req.body.isPublic;
+      const name = req.body.name;
+      if (isPublic === undefined) isPublic = true;
+      if (name === undefined) {
+        res.status(401).json({
+          message: 'Name is not define.'
+        });
+        return;
+      }
+      const roomWithMaxId = await roomDAO.findOne().sort('-roomId');
+      const newRoomId = roomWithMaxId === null ? 1 : roomWithMaxId.roomId + 1;
+      const newRoom = new roomDAO({
+        roomId: newRoomId,
+        creator: req.user._id,
+        player: null,
+        isEnd: false,
+        creatorWinner: 0,
+        playerWinner: 0,
+        draws: 0,
+        createdTime: new Date(),
+        isPublic: isPublic,
+        name: name
+      });
+      await newRoom.save();
+      res.status(200).json({
+        message: 'OK',
+        roomId: newRoom.roomId
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      })
+    }
+  },
+
+  getDetailRoom: async (req, res, next) => {
+    try {
+      const roomId = req.body.roomId;
+      const userId = req.body.userId;
+      if (roomId === undefined || userId === undefined) {
+        res.status(401).json({
+          message: 'Room ID or User ID not defined'
+        });
+        return;
+      }
+
+      const room = await roomDAO.findOne({ roomId: roomId });
+      if (room === null) {
+        res.status(404).json({
+          message: 'Room not found'
+        });
+        return;
+      }
+
+      let creator = {
+        name: 'N/A',
+        mark: 0,
+        draws: 0,
+        cups: 0,
+        wins: 0,
+        loses: 0
+      }
+      let player = {
+        name: 'N/A',
+        mark: 0,
+        draws: 0,
+        cups: 0,
+        wins: 0,
+        loses: 0
+      }
+      if (room.creator !== null) {
+        const _creator = await accountDAO.findById(room.creator);
+        creator = {
+          name: _creator.name,
+          mark: room.creatorWinner,
+          cups: _creator.cups,
+          draws: _creator.draws,
+          wins: _creator.wins,
+          loses: _creator.loses,
+        }
+      }
+
+      if (room.player !== null) {
+        const _player = await accountDAO.findById(room.player);
+        player = {
+          name: _player.name,
+          mark: room.playerWinner,
+          cups: _player.cups,
+          draws: _player.draws,
+          wins: _player.wins,
+          loses: _player.loses,
+        }
+      }
+
+      res.status(200).json({
+        creator: creator,
+        player: player,
+        isCreator: userId === room.creator
+      })
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      });
+    }
+  },
+
+  // Cập nhật thông số của người chơi
+  // Lưu chat, history
+  // Dùng roomId lấy các thông tin có sẵn rồi update thêm history, chat, ...
+  updateParamsAfterEnd: async (req, res, next) => {
+    try {
+      const { id, creatorWins, playerWins, draws, chat } = req.body;
+
+      const room = await roomDAO.findOne({ roomId: id });
+      if (room === null || room === undefined) {
+        res.status(400).json({
+          message: 'Room is not existed'
+        });
+        return;
+      }
+
+      const creator = await accountDAO.findById(room.creator);
+      const player = await accountDAO.findById(room.player);
+      if (creator === null || player === null) {
+        res.status(401).json({
+          message: 'Account is not existed'
+        });
+        return;
+      }
+
+      room.creatorWinner = creatorWins;
+      room.playerWinner = playerWins;
+      room.draws = draws;
+      room.chat = chat;
+      await room.save();
+
+      creator.wins += creatorWins;
+      creator.loses += playerWins;
+      creator.draws += draws;
+      await creator.save();
+
+      player.wins += playerWins;
+      player.loses += creatorWins;
+      player.draws += draws;
+      await player.save();
+
+      res.status(200).json({
+        message: 'OK'
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      })
+    }
+  },
+
+  getRoomByUserId: async (req, res, next) => {
+    try {
+      console.log(req.body);
+      const { id } = req.body;
+      const user = await accountDAO.findById(id);
+      if (user === null) {
+        res.status(400).json({
+          message: 'User is not existed'
+        });
+        return;
+      }
+
+      const rooms = await roomDAO.find({ $or: [{ creator: id }, { player: id }] });
+      res.status(200).json({
+        message: 'OK',
+        rooms: rooms
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      })
+    }
+  },
+
+  getRoomsEnded: async (req, res, next) => {
+    try {
+      const rooms = await roomDAO.find({ isEnd: true });
+      res.status(200).json({
+        message: 'OK',
+        rooms: rooms
+      });
+    } catch (e) {
+      res.status(500).json({
+        message: e.message
+      })
+    }
+  },
+
+  updateResult: async (roomId, result) => {
+    try {
+      //result: 0: draw, 1: creator win, -1: creator lose
+      if (roomId === undefined || result === undefined) {
+        return;
+      }
+
+      const room = await roomDAO.findOne({roomId: roomId});
+      if (room) { 
+        const creator = await accountDAO.findById(room.creator);
+        const player = await accountDAO.findById(room.player);
+        
+        if (creator && player) {
+          let winCups = 0, loseCups = 0;
+          switch (result) {
+            case 0:
+              creator.draws++;
+              player.draws++;
+              break;
+            case 1:
+              winCups = creator.cups >= player.cups ? 1 : 3;
+              loseCups = player.cups > creator.cups ? 3 : 1;
+              creator.wins++;
+              creator.cups += winCups;
+              player.loses++;
+              player.cups -= loseCups;
+              break;
+            case -1:
+              winCups = player.cups >= creator.cups ? 1 : 3;
+              loseCups = creator.cups > player.cups ? 3 : 1;
+              creator.loses++;
+              creator.cups -= loseCups;
+              player.wins++;
+              player.cups += winCups;
+              break;
+          }
+
+          await creator.save();
+          await player.save();
+
+        }
+      }
+    } catch (e) {
+      console.log(e.message);
+    }
+  }
 }
